3d78392d86d9fe62e02d2699bd57585f83a16163
Bradprobert <bradleyprobert@sw.rr.com>
2015-10-01 17:24:18 -0500

    This commit adds a few changes to the GalagaWorld.java class and adds a star class

--- GalagaWorld.java
+++ GalagaWorld.java
@@ -1,5 +1,7 @@
 import greenfoot.*;
 
+import java.util.ArrayList;
+
 /**
  * Write a description of class GalagaWorld here.
  *
@@ -8,14 +10,18 @@ import greenfoot.*;
  */
 public class GalagaWorld extends World {
 
-    GreenfootImage background;
-    /**
-     * Constructor for objects of class GalagaWorld.
-     */
-    public GalagaWorld() {
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1200, 800, 1);
-        background = new GreenfootImage("C:\\Users\\Bradley\\Desktop\\ENGR 1110\\Project\\Galaga\\images\\galaga-background.png");
-        setBackground(background);
-    }
+   GreenfootImage background;
+
+   public GalagaWorld() {
+      // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+      super(1200, 800, 1);
+      background = new GreenfootImage("C:\\Users\\Bradley\\Desktop\\ENGR 1110\\Project\\Galaga\\images\\galaga-background.png");
+      setBackground(background);
+   }
+
+   public void act(){
+
+   }
+
+
 }
