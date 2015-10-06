e670cd65ba06562ef68fb941f786663693f962de
Bradprobert <bradleyprobert@sw.rr.com>
2015-10-06 13:57:10 -0500

    This commit make a few changes to the classes Fighter.java, GalagaWorld.Java, and Star.java

--- BossEnemy.java
+++ BossEnemy.java
@@ -1,19 +1,26 @@
-import greenfoot.*;
+import greenfoot.GreenfootImage;
 
 /**
  * Write a description of class BossEnemy here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class BossEnemy extends Actor
-{
-    /**
-     * Act - do whatever the BossEnemy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+public class BossEnemy extends Enemy {
+
+   private GreenfootImage bossEnemy;
+
+
+   public BossEnemy(int xFinal, int yFinal) {
+      super(xFinal, yFinal);
+      bossEnemy = new GreenfootImage("C:\\Users\\Bradley\\Desktop\\ENGR 1110\\Project\\Galaga\\images\\BossEnemy1.png");
+      bossEnemy.scale(100, 100);
+      setImage(bossEnemy);
+
+   }
+
+   public void act() {
+
+   }
+
 }
--- YellowEnemy.java
+++ YellowEnemy.java
@@ -1,19 +1,20 @@
-import greenfoot.*;
-
 /**
  * Write a description of class YellowEnemy here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class YellowEnemy extends Actor
-{
-    /**
-     * Act - do whatever the YellowEnemy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        
-    }    
+public class YellowEnemy extends Enemy {
+
+   public YellowEnemy(int xFinal, int yFinal) {
+      super(xFinal, yFinal);
+   }
+
+   /**
+    * Act - do whatever the YellowEnemy wants to do. This method is called whenever
+    * the 'Act' or 'Run' button gets pressed in the environment.
+    */
+   public void act() {
+
+   }
 }
