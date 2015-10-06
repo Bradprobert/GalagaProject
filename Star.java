52930b1ccbc697d8272402ef6b7da4d7e87dade2
Bradprobert <bradleyprobert@sw.rr.com>
2015-10-06 13:56:00 -0500

    This commit make a few changes to the classes Fighter.java, GalagaWorld.Java, and Star.java

--- Star.java
+++ Star.java
@@ -1,4 +1,8 @@
-import greenfoot.*;
+import greenfoot.Actor;
+import greenfoot.Greenfoot;
+import greenfoot.GreenfootImage;
+
+import java.awt.*;
 
 /**
  * Write a description of class Star here.
@@ -7,19 +11,50 @@ import greenfoot.*;
  * @version (a version number or a date)
  */
 public class Star extends Actor {
-   GreenfootImage star;
+    GreenfootImage star;
 
-   public Star() {
-      star = new GreenfootImage("star.png");
 
-   }
+    public Star() {
+        star = new GreenfootImage("C:\\Users\\Bradley\\Desktop\\ENGR 1110\\Project\\Galaga\\images\\star.png");
+        star.scale(5, 5);
+        if (Greenfoot.getRandomNumber(100) < 40) {
+            star.setTransparency(80);
+        }
 
-   public void act() {
-      setLocation(this.getX(), this.getY() + 3);
-   }
+        colorStars();
+        setImage(star);
+    }
 
-   @Override
-   public void setLocation(int x, int y) {
-      super.setLocation(x, y);
-   }
+    public void act() {
+        setLocation(this.getX(), this.getY() + 3);
+        if (this.getY() >= getWorld().getHeight() - 2) {
+            setLocation(this.getX(), 0);
+        }
+
+    }
+
+    private void colorStars() {
+        if (Greenfoot.getRandomNumber(100) < 20) {
+            for (int i = 0; i < star.getWidth(); i++) {
+                for (int j = 0; j < star.getHeight(); j++) {
+                    star.setColorAt(i, j, Color.CYAN);
+                }
+            }
+        }
+
+        if (Greenfoot.getRandomNumber(100) < 20) {
+            for (int i = 0; i < star.getWidth(); i++) {
+                for (int j = 0; j < star.getHeight(); j++) {
+                    star.setColorAt(i, j, Color.RED);
+                }
+            }
+        }
+        if (Greenfoot.getRandomNumber(100) < 20) {
+            for (int i = 0; i < star.getWidth(); i++) {
+                for (int j = 0; j < star.getHeight(); j++) {
+                    star.setColorAt(i, j, Color.GREEN);
+                }
+            }
+        }
+    }
 }
